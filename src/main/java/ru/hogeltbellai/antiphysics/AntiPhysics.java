2f9f655bcbb9190e35a743fafd43a485ee568bb4
HogeltBellai <77669576+hogeltbellai@users.noreply.github.com>
2024-02-28 18:14:57 +0300

    Update

---
+++ src/main/java/ru/hogeltbellai/antiphysics/AntiPhysics.java
@@ -0,0 +1,17 @@
+package ru.hogeltbellai.antiphysics;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AntiPhysics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
